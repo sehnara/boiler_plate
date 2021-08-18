75d397521011dc9e7d1bd11c5021a6e905dde11e
sehoon <gsh723@naver.com>
2021-08-18 16:51:09 +0900

    initial commit

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = mongoose.Schema({
+    // contributions : type, maxlength, minlength, unique, trim, default 
+    name:{
+        type : String,
+        maxlengh : 50
+    },
+    email : {
+        type:String,
+        trim : true, // 공백을 없애주는 것 : Trim
+        unique : 1
+    },
+    password:{
+        type: String,
+        minlength : 5
+    },
+    lastname : {
+        type : String,
+        maxlengh : 50
+    },
+    role : {
+        type : Number,
+        default : 0
+    },
+    image: String,
+    token : {
+        type : String
+    },
+    tokenExp:{
+        type : Number
+    }
+});
+const user = mongoose.model('User', userSchema);
+module.exports = {user};
